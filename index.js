da86b01253f183f948fee3a2e1a65a93f73a5d72
Sahruk alam sarder <sahrukalam8059@gmail.com>
2026-05-10 23:38:55 +0600

    Mongodb connection

--- index.js
+++ index.js
@@ -1,13 +1,49 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = 3000
-
+const uri = "mongodb+srv://pawmart-db:pawmart1234@cluster0.jfbqb9o.mongodb.net/?appName=Cluster0";
 app.use(cors())
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+
+    await client.connect();
+    const db=client.db('pawmart-db')
+    const listingsCollection=db.collection('listings') 
+   
+app.get ('/listings',async(req,res)=>{
+  const result=await listingsCollection.find().toArray()
+  console.log(result)
+
+  res.send(result)
+})
+
+
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+  
+   
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
